66d00996a24777430f93d46c6bb78a48e32b6b97
Luis Cardoso <listamino@gmail.com>
2019-05-06 19:46:43 +0100

    first commit

---
+++ src/main/java/tqs/luiscardoso/hw1/Weather.java
@@ -0,0 +1,54 @@
+package tqs.luiscardoso.hw1;
+
+public class Weather {
+    private String summary;
+    private double temperatureHigh, temperatureLow, precipProbability, humidity, pressure;
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public double getTemperatureHigh() {
+        return temperatureHigh;
+    }
+
+    public void setTemperatureHigh(double temperatureHigh) {
+        this.temperatureHigh = temperatureHigh;
+    }
+
+    public double getTemperatureLow() {
+        return temperatureLow;
+    }
+
+    public void setTemperatureLow(double temperatureLow) {
+        this.temperatureLow = temperatureLow;
+    }
+
+    public double getPrecipProbability() {
+        return precipProbability;
+    }
+
+    public void setPrecipProbability(double precipProbability) {
+        this.precipProbability = precipProbability;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+}
